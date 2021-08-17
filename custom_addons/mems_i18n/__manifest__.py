dbf8d54a14a777913db86b7cdd9c7b6eac36745e
appcodeth <appcodetraining@gmail.com>
2021-08-17 14:35:55 +0700

    add base module for i18n

---
+++ custom_addons/mems_i18n/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'MEMs Translation',
+    'version': '0.1',
+    'author': 'Appcode Technology',
+    'website': 'http://www.appcode.co.th/mems',
+    'category': 'Custom',
+    'depends': ['base'],
+    'data': [],
+    'demo': [],
+}
