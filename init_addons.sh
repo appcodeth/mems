963c67698e62320e677ac3633d4dc8a5571da8c7
appcodeth <appcodetraining@gmail.com>
2021-08-24 23:22:44 +0700

    revise startup data

--- init_addons.sh
+++ init_addons.sh
@@ -1,2 +1,2 @@
-python3 build/scripts-3.7/odoo -r odoo13 -w odoo13 --db_host=localhost --db-filter=mems --addons-path="/opt/odoo11/odoo11env/mems/custom_addons"
+python3 build/scripts-3.7/odoo -r odoo13 -w odoo13 --db_host=localhost --db-filter=mems --addons-path="/Users/ekkasit/Project/Mems/mems/custom_addons"
 
