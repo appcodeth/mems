f57e2b351f3455f59eaeefc73bcefb92b0717edb
appcodeth <appcodetraining@gmail.com>
2021-09-05 12:14:50 +0700

    add login controller to separate site

---
+++ custom_addons/login_controller/controllers/controllers.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright 2021 Artem Shurshilov
+
+from odoo import http
+from odoo.http import request
+import werkzeug
+from werkzeug import url_encode
+from odoo import _, SUPERUSER_ID
+from odoo.tools import config
+
+
+class Login(http.Controller):
+    @http.route('/login', type='http', auth='public')
+    def login(self, **kw):
+        site = request.params.get('site')
+        if site and site in http.db_list():
+            request.session['db'] = site
+            return werkzeug.utils.redirect('/web/login')
+        return 'Error! Could not redirect to Website %s' % site
