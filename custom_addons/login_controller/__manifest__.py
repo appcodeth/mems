b8721d56292c1bbd659c0a726fc5eb6aa58b756c
appcodeth <appcodetraining@gmail.com>
2021-09-05 16:07:52 +0700

    revise config controller

---
+++ custom_addons/login_controller/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2021 Artem Shurshilov <shurshilov.a@yandex.ru>
+{
+    'name': "Login in Odoo from URL (by link)",
+    'author': "Shurshilov Artem",
+    'website': "https://eurodoo.com",
+    'category': 'Technical Settings',
+    'version': '14.0.0.0',
+    "license": "LGPL-3",
+    'images': [],
+    'depends': ['base', 'web'],
+    'installable': True,
+}
