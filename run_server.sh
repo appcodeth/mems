ca11e7f746656d74f60b11e7623f93157e9e2391
appcodeth <appcodetraining@gmail.com>
2021-09-05 14:24:49 +0700

    hide login password information

--- run_server.sh
+++ run_server.sh
@@ -2,5 +2,5 @@
   --db_host=localhost \
   --db-filter=basedb \
   --addons-path="/opt/odoo11/odoo11env/mems/odoo/addons,/opt/odoo11/odoo11env/mems/custom_addons" \
-  --update=mems_theme,mems_master,mems_purchase,mems_equipment,mems_pulling,mems_repair,mems_inventory,mems_report,mems_dashboard,mems_i18n \
+  --update=mems_theme,mems_master,mems_purchase,mems_equipment,mems_pulling,mems_repair,mems_inventory,mems_report,mems_dashboard,mems_i18n,login_controller \
   --i18n-overwrite
