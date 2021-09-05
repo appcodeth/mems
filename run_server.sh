05802c363c9989b709cb3d2b1e908ecdd4478028
appcodeth <appcodetraining@gmail.com>
2021-09-05 15:53:41 +0700

    revise config

--- run_server.sh
+++ run_server.sh
@@ -1,7 +1,5 @@
 /opt/odoo11/odoo11env/bin/python3 /opt/odoo11/odoo11env/mems/build/scripts-3.7/odoo -r odoo13 -w odoo13 \
   --db_host=localhost \
-  --db-filter=mems \
   --addons-path="/opt/odoo11/odoo11env/mems/odoo/addons,/opt/odoo11/odoo11env/mems/custom_addons" \
   --update=mems_theme,mems_master,mems_purchase,mems_equipment,mems_pulling,mems_repair,mems_inventory,mems_report,mems_dashboard,mems_i18n,login_controller \
-  --i18n-overwrite --no-database-list
-
+  --i18n-overwrite
