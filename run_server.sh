e9e8bffab0119798cf3bcfb1daf4d809d4940c7a
appcodeth <appcodetraining@gmail.com>
2021-09-05 15:51:50 +0700

    revise config

--- run_server.sh
+++ run_server.sh
@@ -3,4 +3,5 @@
   --db-filter=mems \
   --addons-path="/opt/odoo11/odoo11env/mems/odoo/addons,/opt/odoo11/odoo11env/mems/custom_addons" \
   --update=mems_theme,mems_master,mems_purchase,mems_equipment,mems_pulling,mems_repair,mems_inventory,mems_report,mems_dashboard,mems_i18n,login_controller \
-  --i18n-overwrite
+  --i18n-overwrite --no-database-list
+
