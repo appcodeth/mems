2aeb5b38e0900a9a6a03170fd5416c49b758c6e3
appcodeth <appcodetraining@gmail.com>
2021-09-05 14:27:40 +0700

    hide login password information

--- custom_addons/login_controller/controllers/controllers.py
+++ custom_addons/login_controller/controllers/controllers.py
@@ -10,20 +10,10 @@ from odoo.tools import config
 
 
 class Login(http.Controller):
-    # @http.route('/login', type='http', auth='public')
-    # def login(self, **kw):
-    #     site = request.params.get('site')
-    #     if site and site in http.db_list():
-    #         request.session['db'] = site
-    #         return werkzeug.utils.redirect('/web/login')
-    #     return 'Error! Could not redirect to Website %s' % site
-
-    @http.route('/tranghos', type='http', auth='public')
-    def tranghos(self, **kw):
-        request.session['db'] = 'tranghos'
-        return werkzeug.utils.redirect('/web/login')
-
-    @http.route('/kingnaraihos', type='http', auth='public')
-    def kingnaraihos(self, **kw):
-        request.session['db'] = 'kingnaraihos'
-        return werkzeug.utils.redirect('/web/login')
+    @http.route('/login', type='http', auth='public')
+    def login(self, **kw):
+        site = request.params.get('site')
+        if site and site in http.db_list():
+            request.session['db'] = site
+            return werkzeug.utils.redirect('/web/login')
+        return 'Error! Could not redirect to Website %s' % site
