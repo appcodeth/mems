769c15356c98fd777388bed40b6c5fb4643421f7
appcodeth <appcodetraining@gmail.com>
2021-09-05 16:04:43 +0700

    revise final config

--- run.bat
+++ run.bat
@@ -1,5 +1,4 @@
 python build/scripts-3.7/odoo -r openpg -w openpgpwd ^
-    --db-filter=basedb ^
     --addons-path="D:\Solutions\Mems\mems\custom_addons" ^
-    --update=mems_theme,mems_master,mems_purchase,mems_equipment,mems_pulling,mems_repair,mems_inventory,mems_report,mems_dashboard,mems_i18n,login_controller ^
+    --update=mems_theme,mems_master,mems_purchase,mems_equipment,mems_pulling,mems_repair,mems_inventory,mems_report,mems_dashboard,mems_i18n ^
     --i18n-overwrite
