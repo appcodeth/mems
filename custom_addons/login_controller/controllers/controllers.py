11cdc78297eedc8b4f9ec4d6ac709bfdfb99cc6d
appcodeth <appcodetraining@gmail.com>
2021-09-05 15:46:08 +0700

    add login controller

--- custom_addons/login_controller/controllers/controllers.py
+++ custom_addons/login_controller/controllers/controllers.py
@@ -7,20 +7,10 @@ import werkzeug
 
 
 class Login(http.Controller):
-    # @http.route('/login', type='http', auth='public')
-    # def login(self, **kw):
-    #     site = request.params.get('site')
-    #     if site and site in http.db_list():
-    #         request.session['db'] = site
-    #         return werkzeug.utils.redirect('/web/login')
-    #     return 'Error! Could not redirect to Website %s' % site
-    #
-    @http.route('/tranghos', type='http', auth="none", sitemap=False)
-    def tranghos(self, redirect=None, **kw):
-        request.session['db'] = 'tranghos'
-        return werkzeug.utils.redirect('/web/login')
-
-    @http.route('/kingnaraihos', type='http', auth="none", sitemap=False)
-    def kingnaraihos(self, redirect=None, **kw):
-        request.session['db'] = 'kingnaraihos'
-        return werkzeug.utils.redirect('/web/login')
+    @http.route('/login', type='http', auth='public')
+    def login(self, **kw):
+        site = request.params.get('site')
+        if site and site in http.db_list():
+            request.session['db'] = site
+            return werkzeug.utils.redirect('/web/login')
+        return 'Error! Could not redirect to Website %s' % site
